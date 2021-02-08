fef178e4f5ef21fc618e43c8243eddc3f43a1ffe
twigz826 <thomastwigden@gmail.com>
2021-02-08 13:01:09 -0300

    Javabuzz completed

--- jasmine/jasmine-standalone-3.6.0/spec/FizzbuzzSpec.js
+++ jasmine/jasmine-standalone-3.6.0/spec/FizzbuzzSpec.js
@@ -1,12 +1,27 @@
 describe ("JavaBuzz", function() {
 
-  var fizzbuzz;
+  var number;
+
+  beforeEach(function() {
+    number = new JavaBuzz();
+  });
 
   describe('knows when a number is', function() {
 
-    it ("Returns fizz when the number is divisble by 3", function() {
-      fizzbuzz = new JavaBuzz();
-      expect(fizzbuzz.fizzBuzz(3)).toEqual('fizz');
+    it ("returns fizz when the number is divisble by 3", function() {
+      expect(number.fizzBuzz(6)).toEqual('fizz');
+    });
+
+    it ("returns buzz when the number is divisible by 5", function() {
+      expect(number.fizzBuzz(5)).toEqual('buzz');
+    });
+
+    it ("returns fizzbuzz when the number is divisible by 15", function() {
+      expect(number.fizzBuzz(15)).toEqual('fizzbuzz');
+    });
+
+    it ("returns the number when the number is not divisible by 3, 5 or 15", function() {
+      expect(number.fizzBuzz(2)).toEqual(2);
     });
 
   });
--- jasmine/jasmine-standalone-3.6.0/src/Fizzbuzz.js
+++ jasmine/jasmine-standalone-3.6.0/src/Fizzbuzz.js
@@ -1,7 +1,17 @@
 class JavaBuzz {
 
   fizzBuzz(number) {
-    return 'fizz'
+    if(number % 15 === 0) {
+      return 'fizzbuzz'
+    }
+    else if(number % 5 === 0) {
+      return 'buzz'
+    }
+    else if(number % 3 === 0) {
+      return 'fizz'
+    }
+    else {
+      return number
+    }
   }
-
 }
